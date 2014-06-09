2e1f0f6fc95887b5a6847b16fd87276dbcad4481
dmg <dmg@uvic.ca>
2014-06-09 18:20:46 +0900

    changed license to GPLv2+

--- splitter/splitter.pl
+++ splitter/splitter.pl
@@ -14,16 +14,16 @@
 #    which are under the following license:
 #
 #    This patch is free software; you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
+#    it under the terms of the GNU General Public License as
+#    published by the Free Software Foundation; either version 2 of the
 #    License, or (at your option) any later version.
 #
 #    This patch is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
+#    GNU General Public License for more details.
 #
-#    You should have received a copy of the GNU Affero General Public License
+#    You should have received a copy of the GNU General Public License
 #    along with this patch.  If not, see <http://www.gnu.org/licenses/>.
 
 
