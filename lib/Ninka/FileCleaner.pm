425293b6ffc84164a3397ec95fad0f69f924d3df
René Scheibe <rene.scheibe@gmail.com>
2015-06-01 23:53:47 +0200

    fix indentation for pod format

--- lib/Ninka/FileCleaner.pm
+++ lib/Ninka/FileCleaner.pm
@@ -49,19 +49,19 @@ Escapes apostrophes and other potentially disturbing characters
 
 =head1 COPYRIGHT AND LICENSE
 
-    Copyright (C) 2009-2014  Yuki Manabe and Daniel M. German, 2015 Daniele Fognini and Johannes Najjar
+Copyright (C) 2009-2014  Yuki Manabe and Daniel M. German, 2015 Daniele Fognini and Johannes Najjar
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as
-    published by the Free Software Foundation; either version 2 of the
-    License, or (at your option) any later version.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 2 of the
+License, or (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 =cut
--- lib/Ninka/LicenseRules.pm
+++ lib/Ninka/LicenseRules.pm
@@ -96,19 +96,19 @@ Contains rules used by Ninka::LicenseMatcher.
 
 =head1 COPYRIGHT AND LICENSE
 
-    Copyright (C) 2009-2014  Yuki Manabe and Daniel M. German
+Copyright (C) 2009-2014  Yuki Manabe and Daniel M. German
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as
-    published by the Free Software Foundation; either version 2 of the
-    License, or (at your option) any later version.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 2 of the
+License, or (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 =cut
